2b6cdb3d24ebc2f88b1e412a01acdb27b410e350
josse7cz <josefjanda@email.cz>
2023-07-04 15:14:25 +0200

    Add code to main.py

--- src/utils/main.py
+++ src/utils/main.py
@@ -1,2 +1,11 @@
+import random
 print("Hello world?")
 print("My love is programming")
+
+def volani():
+    list=[]
+    for i in range(10):
+        n = random.randint(1,30)
+        list.append(n)
+    print(list)
+volani()
